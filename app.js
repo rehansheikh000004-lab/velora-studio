727e00d46104a5497a2f28648c25433f03ac1453
rehansheikh000004-lab <rehansheikh000004@gmail.com>
2026-08-24 19:43:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -70,3 +70,14 @@ document.querySelectorAll(".project-card").forEach(card=>{
         card.style.boxShadow="none";
     });
 });
+
+/* AUTO TESTIMONIAL SLIDER */
+
+const testimonials = document.querySelectorAll(".testimonial");
+let current = 0;
+
+setInterval(() => {
+  testimonials[current].classList.remove("active");
+  current = (current + 1) % testimonials.length;
+  testimonials[current].classList.add("active");
+}, 3000);
