4f5f641f1a9c471f42b513b83d089827ffae1026
rehansheikh000004-lab <rehansheikh000004@gmail.com>
2026-08-25 20:08:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -95,3 +95,19 @@ form.addEventListener("submit", (e) => {
 
   form.reset();
 });
+
+/* BACK TO TOP */
+
+const topBtn = document.getElementById("topBtn");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    topBtn.style.display = "block";
+  } else {
+    topBtn.style.display = "none";
+  }
+});
+
+topBtn.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
